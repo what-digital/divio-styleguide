79cd72689e14e362866306c59dae2135638c75ce
Ales Kocjancic <alesdotio@gmail.com>
2012-12-05 09:17:07 +0100

    use the actual view

--- divio_styleguide/urls.py
+++ divio_styleguide/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from divio_styleguide.views import HomeView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='divio_styleguide/home.html'), name='divio_styleguide_home'),
+    url(r'^$', HomeView.as_view(template_name='divio_styleguide/home.html'), name='divio_styleguide_home'),
 )
