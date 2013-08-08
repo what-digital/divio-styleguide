e94176d62c3d621e895ff9beb08b7fe31396224f
Ales Kocjancic <alesdotio@gmail.com>
2013-08-08 13:39:21 +0200

    django 1.3 compatibility

--- divio_styleguide/urls.py
+++ divio_styleguide/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls.defaults import patterns, url
 from divio_styleguide.views import HomeView, FormsView, MessagesView, StaticTemplateView
 
 urlpatterns = patterns('',
