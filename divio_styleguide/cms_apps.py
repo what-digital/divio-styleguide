4fd35c934451f0b0b0e33fa51e8fc5cf7be9b838
Kim Thoenen <kim@smuzey.ch>
2017-02-28 13:48:37 +0100

    adds support for django 1.9

--- divio_styleguide/cms_apps.py
+++ divio_styleguide/cms_apps.py
@@ -1,9 +1,14 @@
+from django.utils.translation import ugettext_lazy as _
+
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
-from django.utils.translation import ugettext_lazy as _
+
 
 class DivioStyleGuideApphook(CMSApp):
     name = _('Divio Style Guide')
-    urls = ['divio_styleguide.urls']
+    app_name = 'divio_styleguide'
+
+    def get_urls(self, *args, **kwargs):
+        return ['divio_styleguide.urls']
 
 apphook_pool.register(DivioStyleGuideApphook)
